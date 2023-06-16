98dbe6444b6c1191bb58d55a15c4b551a8273ee4
Artem <FxD1ag767@outlook.com>
2023-06-16 22:00:27 +0300

    Задание 23 готово

---
+++ task23/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine(" До какого числа выводить ");
+
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+double otv = 1;
+
+if (x > 0)
+{
+    for (int i = 1; i <= x; i++)
+    {
+        otv = Math.Pow(i, 3);
+        Console.Write(otv + " , ");
+
+    }
+}
+else
+{
+    Console.WriteLine("Вы вели не коректное число");
+}
+
+
+
